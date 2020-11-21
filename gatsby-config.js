dba473696fd62c94e30769b7b84a92fd2bc9a714
Wassim Bourguiba <accounts@wassim.co>
2020-11-21 23:41:05 +0100

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Remote Engineer`,
+        description: `Directory of remote engineers.`,
+        author: `@wassimdotco`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
